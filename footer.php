aef99fb9417208381908d4a51254b22ececa65f8
Aditya-Gore22 <152001074+Aditya-Gore22@users.noreply.github.com>
2025-05-11 20:04:45 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,25 @@
+<!-- Footer HTML -->
+<footer>
+  <div class="footer-content">
+    <p>&copy; 2025 Designed by Aditya. All Rights Reserved.</p>
+  </div>
+</footer>
+
+<!-- CSS -->
+<style>
+  footer {
+    background-color: #333;
+    
+    color: white;
+    text-align: center;
+    padding: 5px 0;
+    position: relative;
+    width: 100%;
+    bottom: 0;
+  }
+
+  .footer-content p {
+    font-size: 12px;
+    margin: 0;
+  }
+</style>
